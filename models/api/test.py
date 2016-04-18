3f4ffe25938bb23a52d88f46124306e5cba79805
joseph demarchij <jsd2ej@virginia.edu>
2016-04-18 11:39:40 +0000

    changes

--- models/api/test.py
+++ models/api/test.py
@@ -10,46 +10,49 @@ class ApiTestCase(TestCase):
     fixtures = ['fixtures']
 
     def setup(self):
-    	c = Client()
-    	u1 = models.User(username='test',                         \
+        c = Client()
+        u1 = models.User(username='test',                         \
                     email="test@test",                               \
                     phone='1234567',                               \
                     password=hashers.make_password('testpassword'),  \
                     )
-    	u1.save()
-	
+        u1.save()
+        
+    def test_create_user_get(self):
+        c = Client()
+        response = c.get('api/v1/users/create')
+        print( response)
+        self.assertTrue("must make POST request" in response)
 
-	def test_create_user_get(self):
-		response = c.get('api/v1/users/create')
-		self.assertTrue("must make POST request" in respone)
+    def test_create_user_post_missing_feilds(self):
+        c = Client()
+        response = c.post('api/v1/users/create', {'username' : 'jsd2ej', 'email': 'test@email.com', 'password' : '123'}) 
+        self.assertTrue("missing required fields" in response)
 
 
-	def test_create_user_post_missing_feilds(self):
-		response = c.post('api/v1/users/create', {'username' : 'jsd2ej', 'email': 'test@email.com', 'password' : '123'}) 
- 		self.assertTrue("missing required fields" in respone)
+    def test_create_user_post(self):
+        c = Client()
+        response = c.post('api/v1/users/create', {'username' : 'jsd2ej', 'email': 'test@email.com', 'password' : '123', 'phone' : '1234567'}) 
+        self.assertTrue("user_id" in response)
 
+    def test_find_user_get(self):
+        c = Client()
+        response = c.get('api/v1/users/-1')
+        self.assertTrue("user not found" in response)
 
-	def test_create_user_post(self):
-		response = c.post('api/v1/users/create', {'username' : 'jsd2ej', 'email': 'test@email.com', 'password' : '123', 'phone' : '1234567'}) 
- 		self.assertTrue("user_id" in respone)
+    def test_find_user_get_success(self):
+        c = Client()
+        response = c.get('api/v1/users/1')
+        self.assertTrue("Aaron" in response)
 
- 	def test_find_user_get(self):
- 		respone = c.get('api/v1/users/-1')
- 		self.assertTrue("user not found" in respone)
+    def test_update_user(self):
+        c = Client()
+        response = c.post('api/v1/users/1/update', {
+                        'username':'test1',                         \
+                        'email':"test1@test",                               \
+                        'phone':'1234567',                               \
+                        'password':'testpasswor1d'  
+                          })
+        self.assertTrue('ok' in response)
 
- 	def test_find_user_get_success(self):
- 		respone = c.get('api/v1/users/1')
- 		self.assertTrue("Aaron" in respone)
-
- 	def test_update_user(self):
- 		respone = c.post('api/v1/users/1/update', {
- 				'username'='test1',                         \
-                'email'="test1@test",                               \
-                'phone'='1234567',                               \
-                'password':'testpasswor1d'  
-                })
- 		self.assertTrue('ok' in respone)
-
- 	def test_find_item(self):
- 		respone = 
- 	
+        
--- models/models/settings.py
+++ models/models/settings.py
@@ -86,7 +86,6 @@ DATABASES = {
 	'HOST': 'db',
     }
 }
-
 if 'test' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
     DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
